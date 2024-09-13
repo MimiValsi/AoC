c4e662618c0afd11697fff0b44968b5576c93f13
Miguel Da Silva <miguel@dasilvaf.net>
2024-09-13 22:35:04 +0200

    create day11 file

---
+++ 2015/src/day11.c
@@ -0,0 +1,15 @@
+#include "misc.h"
+
+void part1(char *puzzle);
+void part2(char *puzzle);
+
+int main() {
+        char *puzzle = "hepxcrrq";
+
+        part1(puzzle);
+        // part2(puzzle);
+}
+
+void part1(char *puzzle) {}
+
+void part2(char *puzzle) {}
