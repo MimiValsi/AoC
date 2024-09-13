35dada2833b7eae64948bc92d93bf366fcbe38bc
Miguel Da Silva <miguel@dasilvaf.net>
2024-09-13 22:31:59 +0200

    day10 done

--- 2015/src/day10.c
+++ 2015/src/day10.c
@@ -1,57 +1,107 @@
 #include "misc.h"
+#include <stdio.h>
 
-void part1(char *puzzle);
-void part2(char *puzzle);
+u64 part1(char *puzzle);
+u64 part2(char *puzzle);
 
 int main() {
         char puzzle[1000] = "1113122113";
-        // char puzzle[1000] = "1";
-        part1(puzzle);
-        // part2(puzzle);
+
+        printf("part1: %lu\n", part1(puzzle));
+        part2(puzzle);
+        printf("part2: %lu\n", part2(puzzle));
 }
 
-void part1(char *puzzle) {
-        u64 size = 100000000;
-        char *res = malloc(size);
+u64 part1(char *puzzle) {
+        // random size which is high enough to not realloc
+        u64 size = 400000000;
+        char *res = calloc(size, sizeof(*res));
+        if (!res) {
+                printf("res malloc\n");
+                exit(-1);
+        }
+        // copy 1 time puzzle in res
+        // this var will be reused
         strcpy(res, puzzle);
-        u64 j = 0;
-        u64 track = 0;
+
+        // j counts how many time the same number appear, it's reset each time
+        // the next number isn't the same.
+        // track it means to avoid copying in the same place.
+        // len, just the length of the "new" string
+        u64 j = 0, track = 0, len = 0;
+
         for (u64 o = 0; o < 40; o++) {
-                u64 len = strlen(puzzle);
+                // tmp var which will be created and then copy to res var
+                char *tmp_s = calloc(size, sizeof(*tmp_s));
+                if (!tmp_s) {
+                        printf("malloc tmp_s\n");
+                        exit(-1);
+                }
+
+                len = strlen(res);
                 for (u64 i = 0; i < len; i++) {
                         j++;
                         if (res[i] != res[i + 1]) {
-                                // printf("puzzle: %s\n", puzzle);
-                                // printf("track = %d\n", track);
-                                // printf("j = %d\n", j);
-                                printf("> res = %s\n", res);
-                                res[track++] = (j + '0');
-                                // printf(">> res = %s\n", res);
-                                // printf("j = %d\n", j);
-                                // printf("track = %d\n", track);
-                                res[track++] = res[i];
-                                // printf(">>> res = %s\n", res);
+                                tmp_s[track++] = (j + '0');
+                                tmp_s[track++] = res[i];
                                 j = 0;
                         }
                 }
-                if (strlen(res) > size) {
-                        size *= 1000;
-                        char *tmp = realloc(tmp, size);
-                        if (!tmp) {
-                                exit(-1);
-                        }
-                        res = tmp;
-                }
-                // printf("o = %d\n", o);
-                // printf("> res: %s\n", res);
-                strcpy(res, res);
-                // printf(">> puzzle: %s\n", puzzle);
+                strcpy(res, tmp_s);
                 track = 0;
                 j = 0;
+                free(tmp_s);
         }
-        printf(">>> res = %s\n", res);
-        printf("res len: %lu\n", strlen(res));
+        len = strlen(res);
+
         free(res);
+
+        return len;
 }
 
-void part2(char *puzzle) {}
+u64 part2(char *puzzle) {
+        // random size which is high enough to not realloc
+        u64 size = 400000000;
+        char *res = calloc(size, sizeof(*res));
+        if (!res) {
+                printf("res malloc\n");
+                exit(-1);
+        }
+        // copy 1 time puzzle in res
+        // this var will be reused
+        strcpy(res, puzzle);
+
+        // j counts how many time the same number appear, it's reset each time
+        // the next number isn't the same.
+        // track it means to avoid copying in the same place.
+        // len, just the length of the "new" string
+        u64 j = 0, track = 0, len = 0;
+
+        for (u64 o = 0; o < 50; o++) {
+                // tmp var which will be created and then copy to res var
+                char *tmp_s = calloc(size, sizeof(*tmp_s));
+                if (!tmp_s) {
+                        printf("malloc tmp_s\n");
+                        exit(-1);
+                }
+
+                len = strlen(res);
+                for (u64 i = 0; i < len; i++) {
+                        j++;
+                        if (res[i] != res[i + 1]) {
+                                tmp_s[track++] = (j + '0');
+                                tmp_s[track++] = res[i];
+                                j = 0;
+                        }
+                }
+                strcpy(res, tmp_s);
+                track = 0;
+                j = 0;
+                free(tmp_s);
+        }
+        len = strlen(res);
+
+        free(res);
+
+        return len;
+}
