3de768019d06d20fd87d248c56db766e3666871e
MimiValsi <miguel@dasilvaf.net>
2024-09-16 17:53:21 +0200

    day11 changes

--- 2015/src/day11.c
+++ 2015/src/day11.c
@@ -1,4 +1,5 @@
 #include "misc.h"
+#include <stdio.h>
 
 void part1(char *puzzle);
 void part2(char *puzzle);
@@ -10,6 +11,29 @@ int main() {
         // part2(puzzle);
 }
 
-void part1(char *puzzle) {}
+void part1(char *puzzle) {
+        char res[10] = {0};
+        strcpy(res, puzzle);
+
+        // TODO: increment 3 letters minimun
+        // Must skip 'i', 'o', 'l'
+        // Must contain >= 2 non-overlapping pairs of letters
+
+        printf("start: %s\n", res);
+        u32 len = strlen(res);
+        // printf("res: %c\n", res[0]);
+        // int track = 0;
+        for (int i = 1; i < len; i++) {
+                for (int j = len - 1; res[j] <= 'z'; res[j]++) {
+                        if (res[j] == 'z') {
+                                res[j] = 'a';
+                                res[j - i]++;
+                                break;
+                        }
+                        printf("res: %s\n", res);
+                }
+                printf("> res: %s\n", res);
+        }
+}
 
 void part2(char *puzzle) {}
