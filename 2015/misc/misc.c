ab33cea4b45d636528529c24086a72b2b3876c2d
Miguel Da Silva <miguel@dasilvaf.net>
2024-12-04 20:59:10 +0100

    Indentation & remove sizeof(char) inside malloc()

--- 2015/misc/misc.c
+++ 2015/misc/misc.c
@@ -4,9 +4,7 @@
 /* Read the file, go to the end, return the length and go back to the beginning.
  * Return the length
  */
-u64
-get_file_length(FILE* file)
-{
+u64 get_file_length(FILE *file) {
         fseek(file, 0, SEEK_END);
         u32 length = ftell(file);
         rewind(file);
@@ -16,13 +14,11 @@ get_file_length(FILE* file)
 
 // Use strtok to split a string into words and add them to an array of strings.
 // In this case, a struct with arr[[x]][[y]]
-String
-line_split(char *line, char *delim)
-{
+String line_split(char *line, char *delim) {
         u32 i = 0;
         String str = {0};
         char *token = strtok(line, delim);
-        while(token) {
+        while (token) {
                 strcpy(str.s[i], token);
                 token = strtok(NULL, delim);
                 i++;
@@ -32,15 +28,13 @@ line_split(char *line, char *delim)
 }
 
 /* Read the file, and return line without '\n' */
-char*
-get_line(FILE *file)
-{
-        char *line = malloc(SIZE * sizeof(char) + 1);
+char *get_line(FILE *file) {
+        char *line = malloc(SIZE + 1);
         if (!line) {
                 perror("ERROR: Couldn't allocate memory.");
                 exit(-1);
         }
-        for(i32 i = 0;;i++) {
+        for (i32 i = 0;; i++) {
                 line[i] = fgetc(file);
                 if (line[i] == '\n') {
                         line[i] = '\0';
@@ -52,20 +46,17 @@ get_line(FILE *file)
 }
 
 // check array length.
-u32
-arr_length(i32 *arr) {
-  i32 n = 0;
-  for (i32 i = 0; arr[i] != '\0'; i++) {
-    n++;
-  }
+u32 arr_length(i32 *arr) {
+        i32 n = 0;
+        for (i32 i = 0; arr[i] != '\0'; i++) {
+                n++;
+        }
 
-  return n;
+        return n;
 }
 
 // Check for biggest number in a array
-i32
-max_elem(i32 *arr, u32 size)
-{
+i32 max_elem(i32 *arr, u32 size) {
         i32 max = arr[0];
         for (u32 i = 0; i < size; i++) {
                 if (arr[i] > max) {
@@ -77,9 +68,7 @@ max_elem(i32 *arr, u32 size)
 }
 
 // Check for the lowest number in a array
-i32
-min_elem(i32 *arr, u32 size)
-{
+i32 min_elem(i32 *arr, u32 size) {
         i32 min = arr[0];
         for (u32 i = 0; i < size; i++) {
                 if (arr[i] < min) {
