96e29ab0e10e9e48bb4877eaa1cca17049e796b6
Miguel DA SILVA <miguel@dasilvaf.net>
2024-12-09 20:17:39 +0100

    day12 updates

--- 2015/src/day12.c
+++ 2015/src/day12.c
@@ -1,6 +1,7 @@
 #include "misc.h"
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void part1(FILE *file);
@@ -10,9 +11,11 @@ char *get_all_file(FILE *file, u32 len);
 int sum_numbers(const char *str, u32 i);
 int pos_nb_sum(const char *all, u32 *i);
 int neg_nb_sum(const char *all, u32 *i);
+void zero_arr(char *arr);
 
 int main(void) {
-        FILE *file = fopen("./data/day12.txt", "r");
+        // FILE *file = fopen("./data/day12.txt", "r");
+        FILE *file = fopen("./data/day12_2.txt", "r");
         // part1(file);
         // rewind(file);
         part2(file);
@@ -86,7 +89,6 @@ void part2(FILE *file) {
                                         break;
                                 }
                         }
-                        // int check_inside(const char *str, u32 i);
                         // strstr will search for substring "red" in object
                         // string. if it finds it, it will return the str if not
                         // then it returns (null)
@@ -109,11 +111,46 @@ void part2(FILE *file) {
         printf("total: %d\n", total);
 }
 
-int check_inside(const char *str) {
-        for (u32 j = 1; j < strlen(str); j++) {
+int foo(const char *str) {
+        u32 len = strlen(str);
+        int obj = 0, total = 0, t = 0;
+        char tmp[1000] = {0};
+        bool b[100] = {0};
+
+        for (u32 i = 0; i < len; i++) {
+                if (str[i] == '{') {
+                        bool found = strstr(tmp, "red") ? true : false;
+                        if (found) {
+                                // total = 0;
+
+                                t = 0;
+                                zero_arr(tmp);
+                        }
+                        obj++;
+                }
+
+                tmp[t] = str[i];
+                t++;
+                if (isdigit(str[i])) {
+                        total += pos_nb_sum(str, &i);
+                }
+                if (str[i] == '-') {
+                        total += neg_nb_sum(str, &i);
+                }
+
+                if (str[i] == '}') {
+                        obj--;
+                        t = 0;
+                }
         }
 
         return 0;
+}
+
+void zero_arr(char *arr) {
+        for (int i = 0; i < strlen(arr); i++) {
+                arr[i] = 0;
+        }
 }
 
 int pos_nb_sum(const char *all, u32 *i) {
